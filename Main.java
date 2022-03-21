fec3dd19c340e4e683d1508eb1a06b3e2d63433d
Emre Cerrah <93781753+EmreCerrah@users.noreply.github.com>
2022-03-21 20:28:05 +0300

    Setup Structure

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-}
+        Pokemon charmender= new Pokemon("Charmender",70,10,7);
+        Pokemon squirtle= new Pokemon("Squirtle",90,13,3);
+        Match round= new Match(charmender,squirtle);
+        round.run();
+    }
 }
--- Pokemon.java
+++ Pokemon.java
@@ -1,30 +1,39 @@
 public class Pokemon {
+
     private String name;
     private int health;
     private int ac;
     private int power;
+    protected int dodgeChance;
 
     public Pokemon(String name, int health, int ac, int power) {
         this.name = name;
         this.health = health;
         this.ac = ac;
+        this.dodgeChance=ac;
         this.power = power;
     }
 
-    public void hit(){
-
-
+    public int hit(){
+        int combo= (int) (Math.random() * (10))+power;
+        ac=dodgeChance;
+        return combo;
     }
 
     public void dodge(){
-
-
+        ac+=5;
     }
 
+public void info(){
+    System.out.println("Tur Sırası "+ name);
+    System.out.print("Can : "+health+" ");
+    System.out.println("kaçınma şansı : %"+ac);
 
+}
 
-
-
+    public int getDodgeChance() {
+        return dodgeChance;
+    }
 
     public String getName() {
         return name;
